f665318622406fcf81e0ee22a139a31ffde18d76
Andrey Prokopenko <andrey.prok@gmail.com>
2011-05-28 00:59:13 -0500

    Some extra data for python

--- tools/python/histogramm.py
+++ tools/python/histogramm.py
@@ -2,11 +2,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-N = 13
+N = 12
 # values = (...)
-values = ( 0, 0, 204, 44537, 266367, 120032, 58991, 45252, 40568, 40613, 281932, 223504, 0 )
+values = ( 0, 0, 0, 2, 1826, 104164, 83830, 50000, 34157, 27365, 197341, 623315 )
 
-ticks  = ('1e-6', '1e-5', '1e-4', '1e-3', '1e-2', '1e-1', '0.2', '0.3', '0.4', '0.5', '0.9', '1', 1.0000001)
+ticks  = ('1e-6', '1e-5', '1e-4', '1e-3', '1e-2', '1e-1', '0.2', '0.3', '0.4', '0.5', '0.9', '1')
 
 ind = np.arange(N)  # the x locations for the groups
 width = 1         # the width of the bars
@@ -18,7 +18,7 @@ plt.ylabel('')
 plt.title('')
 plt.xticks(ind+width, ticks )
 
-plt.title('spe')
+plt.title('1-c/d')
 
 def autolabel(rects):
     # attach some text labels
